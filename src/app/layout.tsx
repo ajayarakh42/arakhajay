55ca3a925a7e1b1c6915147606da80e0ea81d373
ajayarakh42 <ajayarakh42@gmail.com>
2026-03-24 19:58:30 +0530

    chatbot changed and updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,17 +21,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </head>
       <body className={`${inter.variable} antialiased`}>
         {children}
 
-        {/* 2. Chatbot script added here, inside the main body */}
-        <Script
-          src="http://localhost:3001/widget.bundle.js"
-          data-chatbot-id="b5b9f7d4-dbbb-44ab-b165-1e6dad2e4d95"
-          strategy="afterInteractive"
-        />
+        {/* 2. Chatbot script replaced properly */}
+        <Script id="chatbot-loader" strategy="afterInteractive">
+          {`
+            (function() {
+              var script = document.createElement('script');
+              script.src = "http://localhost:3001/widget.bundle.js";
+              script.setAttribute('data-chatbot-id', "91670030-b568-43b9-b74d-adf53a6aebc8");
+              script.async = true;
+              document.body.appendChild(script);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
